9e019eb2c99ecdee0c8d98b2d20233a192c31133
RaffyJO <rafiteguh6@gmail.com>
2024-01-06 22:44:41 +0700

    test

--- lib/app/modules/login/controllers/login_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class LoginController extends GetxController {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController reTypePasswordController =TextEditingController();
-}
