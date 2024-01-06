0de127e2b399b8d88148f2ca38aaefa36f3f9c00
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2024-01-06 20:57:55 +0700

    created register

--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-import '../controllers/login_controller.dart';
+import '../controllers/register_controller.dart';
 
-class LoginView extends GetView<LoginController> {
-  const LoginView({Key? key}) : super(key: key);
+class RegisterView extends GetView<RegisterController> {
+  const RegisterView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LoginView'),
+        title: const Text('RegisterView'),
         centerTitle: true,
       ),
       body: const Center(
         child: Text(
-          'LoginView is working',
+          'RegisterView is working',
           style: TextStyle(fontSize: 20),
         ),
       ),
